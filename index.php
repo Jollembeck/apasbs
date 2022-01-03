41204e931cf22aaa157d3d904d424947eb0eed23
Jollembeck <55901340+Jollembeck@users.noreply.github.com>
2022-01-02 22:12:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<meta http-equiv="refresh" content="5; URL='http://www.apasbs.org/'"/>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Meu Redirect</title>
+
+    <meta http-equiv="refresh" content="5; URL='https://www.homehost.com.br/'"/>
+</head>
+<body>
+...
+</body>
+</html>
